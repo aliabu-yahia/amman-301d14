5c711e17ffc7715c73fd93693f59c05d669ca1d5
thamudi <tamim.hamoudi@gmail.com>
2021-03-28 13:39:11 +0300

    class 06 demo shred talk express js

---
+++ class-06/shred-talk-objects/app.js
@@ -0,0 +1,34 @@
+const person =
+{
+    name: "John",
+    role: "Dad",
+    interests: ["Coaching", "Teaching"]
+};
+
+// --- For IN Loop ---
+// for (const key in person) {
+// console.log(key);
+// console.log(typeof (key));
+// console.log(person[key]);
+// console.log(person["name"]);
+// }
+
+// --- Object keys method ---
+// const properties = Object.keys(person);
+// console.log(properties);
+
+// properties.forEach(key => {
+//     console.log(person[key]);
+// });
+
+// --- Object values method
+
+// const values = Object.values(person);
+// console.log(values);
+
+// values.forEach(value => {
+//     console.log(value);
+// });
+
+// const entries = Object.entries(person);
+// console.log(entries);
